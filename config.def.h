8a8094aeacb7e504f249868e0597e652d2ca163a
samugs13 <garciasanchezsamu@gmail.com>
2021-08-13 18:05:06 +0200

    added fuzzy match patch

--- config.def.h
+++ config.def.h
@@ -1,7 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 350;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
